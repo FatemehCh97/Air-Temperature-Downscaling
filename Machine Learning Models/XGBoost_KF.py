df2e69d204e27e4778f0f580e5d47930cb30c26a
Fatemeh Chajaei <88410265+FatemehCh97@users.noreply.github.com>
2024-03-12 19:21:03 +0330

    Update XGBoost Model

--- Machine Learning Models/XGBoost_KF.py
+++ Machine Learning Models/XGBoost_KF.py
@@ -1,113 +1,108 @@
-# -*- coding: utf-8 -*-
 """
 @author: FatemehChajaei
 """
 
-
-from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBRegressor
-from sklearn.model_selection import GridSearchCV, KFold, cross_val_predict
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from sklearn.model_selection import KFold, cross_val_predict
 import pandas as pd
-import numpy as np
 import time
 import os
 
-param_path = r"D:\Code\Amsterdam Parameter\Train_Test\RoI"
-save_path = r"D:\Code\Temp_Prediction_Amsterdam\RoI\Models\save_models"
+from my_functions import preprocess_data, perform_grid_search
+from my_functions import evaluate_performance
 
-df = pd.read_csv(os.path.join(param_path, "Train_Parameters_DataFrame.csv"))
-df_test = pd.read_csv(os.path.join(param_path, "Test_Parameters_DataFrame.csv"))
+# Set paths
+param_path = "Train_Test/RoI"
 
-# df_train_max = pd.read_csv(os.path.join(param_path, "Parameters_DataFrame_max.csv"))
-# df_test_max = pd.read_csv(os.path.join(param_path, "Test_Parameters_DataFrame_max.csv"))
+# Load training and testing data for estimating daily average temperature
+df_train = pd.read_csv(os.path.join(param_path,
+                                    "Train_Parameters_DataFrame.csv"))
+df_test = pd.read_csv(os.path.join(param_path,
+                                   "Test_Parameters_DataFrame.csv"))
 
-# df_train_min = pd.read_csv(os.path.join(param_path, "Parameters_DataFrame_min.csv"))
-# df_test_min = pd.read_csv(os.path.join(param_path, "Test_Parameters_DataFrame_min.csv"))
+# Load training and testing data for estimating daily maximum temperature
+df_train_max = pd.read_csv(os.path.join(param_path,
+                                        "Parameters_DataFrame_max.csv"))
+df_test_max = pd.read_csv(os.path.join(param_path,
+                                       "Test_Parameters_DataFrame_max.csv"))
 
+# Load training and testing data for estimating daily minimum temperature
+df_train_min = pd.read_csv(os.path.join(param_path,
+                                        "Parameters_DataFrame_min.csv"))
+df_test_min = pd.read_csv(os.path.join(param_path,
+                                       "Test_Parameters_DataFrame_min.csv"))
 
+# Preprocess the data
+X_train, y_train, X_test, y_test = preprocess_data(df_train, df_test)
+X_train_max, y_train_max, X_test_max, y_test_max = preprocess_data(
+    df_train_max, df_test_max
+    )
+X_train_min, y_train_min, X_test_min, y_test_min = preprocess_data(
+    df_train_min, df_test_min
+)
 
-X = df[df.columns[1:15]]
-X_test = df_test[df_test.columns[1:15]]
-
-
-df_all = np.concatenate((X, X_test))
-
-
-scaler = MinMaxScaler()
-Xs = scaler.fit_transform(df_all)
-
-y = df.loc[:,['Temp']]
-y = y.to_numpy()
-y = np.ravel(y)
-Xtrain = Xs[0:8060]
-
-y_test = df_test.loc[:,['Temp']]
-y_test = y_test.to_numpy()
-y_test = np.ravel(y_test)
-Xtest = Xs[8060:]
-
+# Define XGBoost model
 model = XGBRegressor()
 
-"""Grid search-Kfold"""
-distributions = dict(n_estimators = [100, 250, 500, 750, 1000], 
-                      max_depth=[3, 5, 7, 10],
-                      eta=[0.001, 0.01, 0.1],
-                      min_child_weight=[1, 3, 5, 10],
-                      subsample=[0.5, 0.75, 1],
-                      colsample_bytree=[0.5, 0.75, 1])
+# Define hyperparameter grid for Grid Search
+param_grid = dict(n_estimators=[100, 250, 500, 750, 1000],
+                  max_depth=[3, 5, 7, 10],
+                  eta=[0.001, 0.01, 0.1],
+                  min_child_weight=[1, 3, 5, 10],
+                  subsample=[0.5, 0.75, 1],
+                  colsample_bytree=[0.5, 0.75, 1])
 
-reg = GridSearchCV(model, distributions, verbose=2)
-search = reg.fit(Xtrain, y)
-print('----------------------------')
-print(search.best_params_)
-print('----------------------------')
+# Perform Grid Search
+best_params_avg = perform_grid_search(model, param_grid,
+                                      X_train, y_train)
+best_params_max = perform_grid_search(model, param_grid,
+                                      X_train_max, y_train_max)
+best_params_min = perform_grid_search(model, param_grid,
+                                      X_train_min, y_train_min)
 
+model_avg = XGBRegressor(**best_params_avg)
+model_max = XGBRegressor(**best_params_max)
+model_min = XGBRegressor(**best_params_min)
 
-
-# model = XGBRegressor(n_estimators = search.best_params_['n_estimators'],
-#                       max_depth = search.best_params_['max_depth'],
-#                       eta = search.best_params_['eta'],
-#                       min_child_weight = search.best_params_['min_child_weight'],
-#                       subsample = search.best_params_['subsample'],
-#                       colsample_bytree = search.best_params_['colsample_bytree'])
-
-
-
-model = XGBRegressor(n_estimators = 1000, 
-                      max_depth=5,
-                      eta= 0.1,
-                      min_child_weight=5,
-                      subsample=0.75,
-                      colsample_bytree=0.75)
-
-
-
+# Use KFold cross-validation
 cv = KFold(n_splits=10, random_state=1, shuffle=True)
 
+# Train and predict on the training set
 start = time.time()
-df_test["y_pred"] = cross_val_predict(model, Xtest, y_test, cv=cv)
-print("Time elapsed: ", time.time() - start)
+df_train["y_pred"] = cross_val_predict(model_avg, X_train,
+                                       y_train, cv=cv)
+print("Time elapsed for training (Average Temp): ", time.time() - start)
 
 start = time.time()
-df["y_pred"] = cross_val_predict(model, Xtrain, y, cv=cv)
-print("Time elapsed: ", time.time() - start)
+df_train_max["y_pred"] = cross_val_predict(model_max, X_train_max,
+                                           y_train_max, cv=cv)
+print("Time elapsed for training (Max Temp): ", time.time() - start)
 
+start = time.time()
+df_train_min["y_pred"] = cross_val_predict(model_min, X_train_min,
+                                           y_train_min, cv=cv)
+print("Time elapsed for training (Min Temp): ", time.time() - start)
 
-RMSE_train = np.round(mean_squared_error(df["Temp"], df["y_pred"], squared=False),3)
-MAE_train = np.round(mean_absolute_error(df["Temp"], df["y_pred"]),3)
-RMSE_test = np.round(mean_squared_error(df_test["Temp"], df_test["y_pred"], squared=False),3)
-MAE_test = np.round(mean_absolute_error(df_test["Temp"], df_test["y_pred"]),3)
-print ("RMSE Train: ", RMSE_train, "      RMSE Test: ", RMSE_test)
-print ("MAE Train:  ", MAE_train, "      MAE Test: ", MAE_test)
+# Train and predict on the test set
+start = time.time()
+df_test["y_pred"] = cross_val_predict(model_avg, X_test, y_test, cv=cv)
+print("Time elapsed for testing (Average Temp): ", time.time() - start)
 
-APE = (np.abs(df_test["Temp"]-df_test["y_pred"]))/(df_test["Temp"])
-MAPE = mean_absolute_percentage_error(df_test["Temp"], df_test["y_pred"])
-print("MAPE: ", MAPE)
+start = time.time()
+df_test_max["y_pred"] = cross_val_predict(model_max, X_test_max,
+                                          y_test_max, cv=cv)
+print("Time elapsed for testing (Max Temp): ", time.time() - start)
 
-from sklearn.metrics import r2_score
-r2 = r2_score(df_test["Temp"], df_test["y_pred"])
-print('r2 score for perfect model is', r2)
+start = time.time()
+df_test_min["y_pred"] = cross_val_predict(model_min, X_test_min,
+                                          y_test_min, cv=cv)
+print("Time elapsed for testing (Min Temp): ", time.time() - start)
 
+# Evaluate performance
+evaluate_performance(df_train, "Train (Average Temp)")
+evaluate_performance(df_train_max, "Train (Max Temp)")
+evaluate_performance(df_train_min, "Train (Min Temp)")
 
-
+evaluate_performance(df_test, "Test (Average Temp)")
+evaluate_performance(df_test_max, "Test (Max Temp)")
+evaluate_performance(df_test_min, "Test (Min Temp)")
